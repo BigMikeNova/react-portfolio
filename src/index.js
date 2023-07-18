7971bd7a0c71862bac2ce844edd9d33c0f5f724d
BigMikeNova <125073452+BigMikeNova@users.noreply.github.com>
2023-07-17 20:35:35 -0400

    file restructure

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import './index.css';
-import Header from './components/header';
-import Navigation from './components/navigation';
-import Project from './components/project';
-import Footer from './components/footer';
+import Header from '../components/header';
+import Navigation from '../components/navigation';
+import Project from '../components/project';
+import Footer from '../components/footer';
 
 const App = () => {
   return (
